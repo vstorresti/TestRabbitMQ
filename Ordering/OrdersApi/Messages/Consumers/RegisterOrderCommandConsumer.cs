1f82f6688275834fbedb4c97d5ee962f5bd752cb
vstorresti <vstorresti@gmail.com>
2021-03-07 22:18:31 -0300

    Adicionando Order

---
+++ Ordering/OrdersApi/Messages/Consumers/RegisterOrderCommandConsumer.cs
@@ -0,0 +1,17 @@
+﻿using Faces.SharedLib.Messaging.InterfacesConstants.Commands;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrdersApi.Messages.Consumers
+{
+    public class RegisterOrderCommandConsumer : IConsumer<IRegisterOrderCommand>
+    {
+        public Task Consume(ConsumeContext<IRegisterOrderCommand> context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
